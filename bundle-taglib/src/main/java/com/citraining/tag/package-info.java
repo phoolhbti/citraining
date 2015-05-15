eba431e389dffc76851532d94b7d47d22380b49e
Phool Chandra <phoolhbti@gmail.com>
2015-05-15 16:45:06 +0530

    Create package-info.java

---
+++ bundle-taglib/src/main/java/com/citraining/tag/package-info.java
@@ -0,0 +1,6 @@
+/*
+ * A package information file in which specify the tld name
+ * */
+@aQute.bnd.annotation.Version("1.0.0")
+@tldgen.TagLibrary(value = "http://www.citraining.com/hello", descriptorFile = "hello.tld")
+package com.citraining.tag;
