5a2bd411702bc0dd7e3359e21476c650170c67d8
Phool Chandra <phoolhbti@gmail.com>
2015-05-15 16:48:40 +0530

    Create package-info.java

---
+++ bundle-taglib/src/main/java/com/citraining/tags/wcmmode/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * @author PC
+ *
+ */
+@aQute.bnd.annotation.Version("1.0.0")
+@tldgen.TagLibrary(value = "http://www.citraining.com/wcmmode", descriptorFile = "wcmmode.tld")
+package com.citraining.tags.wcmmode;
